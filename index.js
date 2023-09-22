e5818a1b53f7f2b39d78eb047c0578231c1347a2
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-09-22 21:40:01 +0530

    Added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config()
 
 const app = express();
 app.use(express.json());
-// app.use(cors())
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.status(200).send({ msg: "You are welcome to Home Page !" });
